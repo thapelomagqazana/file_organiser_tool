0fd934a4e6b4ddd49e418c3105143eecf1cfb9cb
thapelomagqazana <tapsmcgzee8@gmail.com>
2023-11-15 23:42:56 +0200

    Integrated logging to the codebase

--- src/file_organizer.py
+++ src/file_organizer.py
@@ -1,11 +1,12 @@
 import os
 import shutil
 from rule_parser import RuleParser
+import logging
 
 class FileOrganizer:
     """Organizes files based on the defined rules."""
 
-    def __init__(self):
+    def __init__(self, log_file="file_organizer.log"):
        """
         Initialize FileOrganizer by prompting the user for input.
 
@@ -16,6 +17,17 @@ class FileOrganizer:
        self.source_dir = os.path.expanduser(input("Enter the source directory 'e.g ~/Documents': ")) 
        self.destination_dir = os.path.expanduser(input("Enter the destination directory 'e.g ~/Pictures': "))
        self.rule_parser = RuleParser(self.get_rules())
+       self.log_file = log_file
+       self.setup_logging()
+    
+
+    def setup_logging(self):
+        logging.basicConfig(
+            filename=self.log_file,
+            level=logging.INFO,
+            format="%(asctime)s - %(levelname)s - %(message)s",
+            datefmt="%Y-%m-%d %H:%M:%S"
+        )
 
 
     def get_rules(self):
@@ -36,7 +48,7 @@ class FileOrganizer:
             user_confirmation = input("Confirm re-organizing files? (y/n): ").lower()
             
             if user_confirmation == "y":
-
+                logging.info("File organization started...")
                 print("File organization started...\n")
                 count = 1
                 for filename in os.listdir(self.source_dir):
@@ -49,12 +61,16 @@ class FileOrganizer:
 
                         shutil.move(source_path, os.path.join(destination_path, filename))
                         print(f"File_count : {count} - (Moved '{filename}' to '{destination_folder}')")
+                        logging.info(f"File_count : {count} - (Moved '{filename}' to '{destination_folder}')")
                         count += 1
 
                 print("\nFile organization completed successfully.")
+                logging.info("\nFile organization completed successfully.")
                     
                         
             else:
                 print("Operation cancelled!")
+                logging.info("Operation cancelled!")
         except Exception as e:
             print(f"Error organizing files: {e}")
+            logging.error(f"Error organizing files: {e}")
